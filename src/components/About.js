9bc00a01a2fb46702de0869b42b06d89719ec6ad
bbiijjeett <bijeetnathbtech@gmail.com>
2023-10-04 00:41:03 +0530

    Odia text check

--- src/components/About.js
+++ src/components/About.js
@@ -14,13 +14,13 @@ const About = () => {
         <article className="about-info">
           <h3>explore the difference</h3>
           <p>
-            Welcome to CloudTrail, where unforgettable journeys await! We are
-            more than just a tour company; we are your gateway to discovering
-            the world's most captivating destinations and creating lasting
-            memories. Established with a passion for travel and a commitment to
-            excellence, CloudTrail is dedicated to providing you with
-            extraordinary experiences that will leave you inspired and eager for
-            more.
+            CloudTrail କୁ ସ୍ Welcome ାଗତ, ଯେଉଁଠାରେ ଅବିସ୍ମରଣୀୟ ଯାତ୍ରା ଅପେକ୍ଷା
+            କରିଥାଏ! ଆମେ ମାନେ କେବଳ ଏକ ଟୁର୍ କମ୍ପାନୀଠାରୁ ଅଧିକ; ଆମେ ଆବିଷ୍କାର କରିବା
+            ପାଇଁ ତୁମର ଗେଟୱେ | ଦୁନିଆର ସବୁଠାରୁ ଆକର୍ଷଣୀୟ ଗନ୍ତବ୍ୟସ୍ଥଳ ଏବଂ ସ୍ଥାୟୀ
+            ସୃଷ୍ଟି | ସ୍ମୃତି ଭ୍ରମଣ ପାଇଁ ଏକ ଉତ୍ସାହ ଏବଂ ଏକ ପ୍ରତିବଦ୍ଧତା ସହିତ
+            ପ୍ରତିଷ୍ଠିତ | ଉତ୍କର୍ଷତା, କ୍ଲାଉଡ୍ ଟ୍ରେଲ୍ ଆପଣଙ୍କୁ ଯୋଗାଇବା ପାଇଁ
+            ଉତ୍ସର୍ଗୀକୃତ | ଅସାଧାରଣ ଅନୁଭୂତି ଯାହା ଆପଣଙ୍କୁ ପ୍ରେରଣା ଯୋଗାଇବ ଏବଂ ଆଗ୍ରହୀ
+            କରିବ | ଅଧିକ
           </p>
           <a href="#about" className="btn">
             read more
